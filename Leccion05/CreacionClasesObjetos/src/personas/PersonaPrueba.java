ef6a87e21235213946eee2998781e8a7bdd5e307
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-02-23 18:15:20 -0300

    Sección: 9 - Fundamentos Java: Creación de Objetos en Java

---
+++ Leccion05/CreacionClasesObjetos/src/personas/PersonaPrueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package personas;
+
+/**
+ *
+ * @author qa-ntb
+ */
+public class PersonaPrueba {
+    public static void main(String args[]){
+    Persona p1 = new Persona();
+    System.out.println("Valores por default del objetoPersona");
+    //p1.desplegarNombre();
+    
+    }
+}
