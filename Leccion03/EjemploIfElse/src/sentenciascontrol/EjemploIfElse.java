e759f5223e22217639cea7afc9ebd9643c05f8f1
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-02-26 14:57:07 -0300

    Sección: 6 - Fundamentos Java: Sentencias de Control 29. Ejercicio: Manejo de if-else

---
+++ Leccion03/EjemploIfElse/src/sentenciascontrol/EjemploIfElse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sentenciascontrol;
+
+/**
+ *
+ * @author qa-ntb
+ */
+public class EjemploIfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int x = 40;
+        if (x < 20){
+            System.out.println("X menos que 20\n");
+        }else{
+        System.out.println("X mayor que 20\n");
+    } // TODO code application logic here
+    }
+    
+}
