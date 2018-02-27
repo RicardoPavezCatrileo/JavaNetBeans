f4dc73ee4021db1509af754936142ee51e0e9ba4
Ricardo Pavez Catrileo <ricardoalfredo.pavez@gmail.com>
2018-02-27 19:29:15 -0300

    Conclusion del Ejercicio * Con este Ejercicio hemos puesto en practica el manejo de constructores en Java. * Los Constructores nos permiten crear nuestros objetos con valores personalizados desde el momento de la creacion, de esta manera. * Un Constructores un tipo de metodo que solo se puede utilizar al momento de crear un objeto y debe llamarse igual que la clase.

--- Leccion06/Aritmetica_v2/src/aritmetica/PruebaAritmetica.java
+++ Leccion06/Aritmetica_v2/src/aritmetica/PruebaAritmetica.java
@@ -26,8 +26,8 @@ public class PruebaAritmetica {
         System.out.println("\nResultado suma atributos obj2:" + obj2.sumar() );
         
         //Conclusion del Ejercicio
-//* Con este Ejercicio hemos puesto en practica el manejo de constructores en Java.
-//* Los Constructores nos permiten crear nuestros objetos con valores personalizados desde el momento de la creacion, de esta manera.
-//* Un Constructores un tipo de metodo que solo se puede utilizar al momento de crear un objeto y debe llamarse igual que la clase.
+        //* Con este Ejercicio hemos puesto en practica el manejo de constructores en Java.
+        //* Los Constructores nos permiten crear nuestros objetos con valores personalizados desde el momento de la creacion, de esta manera.
+        //* Un Constructores un tipo de metodo que solo se puede utilizar al momento de crear un objeto y debe llamarse igual que la clase.
     }
 }
