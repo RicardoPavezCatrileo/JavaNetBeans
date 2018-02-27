9a31b2ecb7d66371644b9769af30b9ef3210dd92
Ricardo Pavez Catrileo <ricardoalfredo.pavez@gmail.com>
2018-02-27 18:25:01 -0300

    CreacionClasesObjetos

--- Leccion05/CreacionClasesObjetos/src/personas/Persona.java
+++ Leccion05/CreacionClasesObjetos/src/personas/Persona.java
@@ -10,5 +10,20 @@ package personas;
  * @author qa-ntb
  */
 public class Persona {
+   //Atributos de una clase
+    //No es necesario asignar valores
+    String nombre;
+    String apellidoPaterno;
+    String apellidoMaterno;
+
+    //Metodos de la clase
+    //Los usaran los objetos de esta clase
+    public void desplegarNombre() {
+        System.out.println("Nombre : " + nombre);
+        System.out.println("Apellido Paterno : " + apellidoPaterno);
+        System.out.println("Apellido Materno : " + apellidoMaterno);
+    }
+    
+    
     
 }
--- Leccion05/CreacionClasesObjetos/src/personas/PersonaPrueba.java
+++ Leccion05/CreacionClasesObjetos/src/personas/PersonaPrueba.java
@@ -10,10 +10,22 @@ package personas;
  * @author qa-ntb
  */
 public class PersonaPrueba {
-    public static void main(String args[]){
-    Persona p1 = new Persona();
-    System.out.println("Valores por default del objetoPersona");
-    //p1.desplegarNombre();
-    
+     public static void main(String args[]){
+        //Creacion de un objeto
+        Persona p1 = new Persona();
+        
+        //LLamando a un metodo del objeto creado
+        System.out.println("Valores por default del objeto Persona");
+        p1.desplegarNombre();
+        
+        //Modificar valores
+        p1.nombre = "Juan";
+        p1.apellidoPaterno = "Esparza";
+        p1.apellidoMaterno = "Lara";
+        
+        //Volvemos a llamar al metodo
+        System.out.println("\nNuevos valores del objeto Persona");
+        p1.desplegarNombre();
+        
     }
 }
