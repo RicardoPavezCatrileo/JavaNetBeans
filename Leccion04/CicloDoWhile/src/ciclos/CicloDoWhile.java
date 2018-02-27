3a21f458f4475426f481032605539eae79893ccd
Ricardo Pavez Catrileo <ricardoalfredo.pavez@gmail.com>
2018-02-27 17:36:53 -0300

    CicloDoWhile

---
+++ Leccion04/CicloDoWhile/src/ciclos/CicloDoWhile.java
@@ -0,0 +1,15 @@
+package ciclos;
+/**
+ * @author RicardoPavezCatrileo
+ */
+public class CicloDoWhile {
+    public static void main(String[] args) {
+        int contador = 0;
+        int limite = 10;
+        do{
+            System.out.println("contador = "+ contador);
+            contador++;
+        }while(contador < limite);
+    }
+    
+}
