cf5aa46fb4dd21251ff275f3b0aed7760d264910
Ricardo Pavez Catrileo <ricardoalfredo.pavez@gmail.com>
2018-02-27 16:11:27 -0300

    Sección: 6 - Fundamentos Java: Sentencias de Control 31. Ejercicio: Ejercicio Switch

--- Leccion03/CalculaMesSwitch/src/sentenciascontrol/CalculaMesSwitch.java
+++ Leccion03/CalculaMesSwitch/src/sentenciascontrol/CalculaMesSwitch.java
@@ -7,7 +7,7 @@ package sentenciascontrol;
 
 /**
  *
- * @author rpavez
+ * @author RicardoPavezCatrileo
  */
 public class CalculaMesSwitch {
 
@@ -15,7 +15,27 @@ public class CalculaMesSwitch {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int mes = 12; // Mes del año
+        String estacion;
+
+        String saludo = "Hola Mundo";
+        
+        switch(mes){
+            case 1:case 2: case 12:
+                estacion = "Invierno";
+                break;
+            case 3: case 4: case 5:
+                estacion = "Primavera";
+                break;
+            case 6: case 7: case 8:
+                estacion = "Verano";
+                break;
+            case 9: case 10 : case 11:
+                estacion = "Otoño";
+                break;
+            default:
+            estacion = "Mes incorrecto";
+        }
+        System.out.println("La estacion para el mes " + mes + " es: " + estacion);
     }
-    
 }
