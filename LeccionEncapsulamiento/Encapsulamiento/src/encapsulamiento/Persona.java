cc186db0300f931b51e0b6cbb3501e277e3ec184
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-05 15:26:48 -0300

    Demo Encapsulamiento

---
+++ LeccionEncapsulamiento/Encapsulamiento/src/encapsulamiento/Persona.java
@@ -0,0 +1,59 @@
+package encapsulamiento;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author qa-ntb
+ */
+public class Persona {
+    //Constructor Vacio
+    public Persona() {
+    }
+    //Constructor con 3 argumentos
+    public Persona(String nombre, String apellido, boolean borrado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.borrado = borrado;
+    }
+
+    //Atributos privados
+    private String nombre;
+    private String apellido;
+    private boolean borrado;
+
+    //Métodos publicos para acceder y/o modificar los atributos
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public boolean isBorrado() {
+        return borrado;
+    }
+
+    public void setBorrado(boolean borrado) {
+        this.borrado = borrado;
+    }
+    
+    //Método que imprime el estado del objeto
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", borrado=" + borrado + '}';
+    }
+}
