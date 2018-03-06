b31457d0f7abddd141376125cd40e893894bfbdd
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-06 19:10:55 -0300

    Ejemplo Paquetes

---
+++ Leccion22/EjemploPaquetes/src/ejemplopaquetes/EjemploPaquetes.java
@@ -0,0 +1,18 @@
+package ejemplopaquetes;
+
+import static com.gm.Utileria.imprimir;
+        /**
+ *
+ * @author qa-ntb
+ */
+public class EjemploPaquetes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        imprimir("Juan");
+        // TODO code application logic here
+    }
+    
+}
