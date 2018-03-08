a3199acb2cc25b7fba7c0d7640a48be9154d5c6c
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-08 18:37:07 -0300

    Proyecto Caja(Alto-Ancho-Profundo)

---
+++ FundamentosJava/Leccion06/ProyectoCaja/src/proyectocaja/ProyectoCaja.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectocaja;
+
+/**
+ *
+ * @author qa-ntb
+ */
+public class ProyectoCaja {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Variables locales
+        int operandoAlto = 6;
+        int operandoBajo = 2;
+        int operandoProfundo = 3;
+    }
+    
+}
