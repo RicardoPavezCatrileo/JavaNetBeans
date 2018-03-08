d4abb3d18da58027e9ea61d20299659e1f9c1205
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-08 10:50:52 -0300

    Cambios en los Tags de @version V.0.0.0.1

--- Leccion23/EjemploPalabraFinal/src/ejemplopalabrafinal/EjemploPalabraFinal.java
+++ Leccion23/EjemploPalabraFinal/src/ejemplopalabrafinal/EjemploPalabraFinal.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplopalabrafinal;
 
 /**
  *
- * @author qa-ntb
+ * @author RicardoPavezCatrileo
+ * @version V.0.0.0.1
  */
 public class EjemploPalabraFinal {
 
