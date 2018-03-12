8a28002257e13c067436cb4bc20409bb67f569cb
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-12 17:24:34 -0300

    Cambios de Carpetas

---
+++ FundamentosJava/EjemploHerencia/src/ejemploherencia/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploherencia;
+
+/**
+ *
+ * @author qa-ntb
+ */
+public class Persona {
+    private String nombre;
+    private char genero;
+    private int edad;
+    private String direccion;
+    
+    //constructor vacio
+    public Persona(){}
+    
+    //constructor 1 argumento
+    public Persona(String nombre){
+    this.nombre = nombre;
+    }
+    //constructor completo
+    public Persona (String nombre,char genero,int edada,String direccion){
+    this.nombre=nombre;
+    this.genero=genero;//M-masculino,F-Femenino
+    }
+}
