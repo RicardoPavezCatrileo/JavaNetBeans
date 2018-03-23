cb3f632cebc82373d5bad6a3ddc02a018862a986
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-23 11:33:33 -0300

    HolaMundoEJB

--- ProgramacionJava/Leccion10/EjemploSobreescritura/src/ejemplosobreescritura/EjemploSobreescritura.java
+++ ProgramacionJava/Leccion10/EjemploSobreescritura/src/ejemplosobreescritura/EjemploSobreescritura.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejemplosobreescritura;
 
-/**
- *
- * @author qa-ntb
- */
 public class EjemploSobreescritura {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Empleado empleado = new Empleado("Juan",1000);
+        System.out.println( empleado.obtenerDetalles() );
+        
+        Gerente gerente = new Gerente("Karla",2000,"Finanzas");
+        System.out.println( gerente.obtenerDetalles() );
     }
-    
 }
