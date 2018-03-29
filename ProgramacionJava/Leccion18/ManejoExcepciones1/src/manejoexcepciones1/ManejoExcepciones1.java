ca65b3fc455406f41814e3b37d3df419240f1eae
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-03-29 09:45:24 -0300

    ManejoExcepciones1

---
+++ ProgramacionJava/Leccion18/ManejoExcepciones1/src/manejoexcepciones1/ManejoExcepciones1.java
@@ -0,0 +1,19 @@
+package manejoexcepciones1;
+
+import domain.*;
+
+public class ManejoExcepciones1 {
+
+    public static void main(String[] args) {
+
+        try {
+            Division division = new Division(10, 0);
+            division.visualizarOperacion();
+        } catch (OperationException oe) {
+            System.out.println("OcurriÃ³ un error!!!");
+            oe.printStackTrace();
+        }
+
+    }
+
+}
