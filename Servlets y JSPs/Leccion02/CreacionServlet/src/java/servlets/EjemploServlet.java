d243f7c2b12c26230e0a6995a414a1a390f34ca1
ricardoalfredo.pavez@gmail.com <ricardoalfredo.pavez@gmail.com>
2018-04-09 14:30:21 -0300

    CreacionServlet

--- Servlets y JSPs/Leccion02/CreacionServlet/src/java/servlets/EjemploServlet.java
+++ Servlets y JSPs/Leccion02/CreacionServlet/src/java/servlets/EjemploServlet.java
@@ -10,7 +10,6 @@ public class EjemploServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
-        
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
